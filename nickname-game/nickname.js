d6917920344bb2073f1267e029ddb2b3d8db499a
Jaime Segura <jseguraweb@gmail.com>
2019-08-27 22:46:58 +0200

    add goal of the project

---
+++ nickname-game/nickname.js
@@ -0,0 +1,5 @@
+// NICKNAME GAME
+
+// The goal of this JS project is to return the user a random nickname which begins with the same letter that his/her name does.
+// For this I will store 5 nicknames per letter of the alphabet.
+
