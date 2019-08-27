d996d6f87b27b677026a59820c89b4836cd26128
Jaime Segura <jseguraweb@gmail.com>
2019-08-27 23:25:26 +0200

    add function validate to run the game since the user clicks on submit

--- nickname-game/script-nickname.js
+++ nickname-game/script-nickname.js
@@ -14,10 +14,38 @@ function giveNicknameWithLetterA() {
     return nicknameLetterA[Math.floor(Math.random() * 5)];
 }
 
+function giveNicknameWithLetterB() {
+    return nicknameLetterB[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterC() {
+    return nicknameLetterC[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterD() {
+    return nicknameLetterD[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterE() {
+    return nicknameLetterE[Math.floor(Math.random() * 5)];
+}
+
 function validate(userName) {
     if (userName[0] === "A") {
         return `${giveNicknameWithLetterA()} ${userName}`;
+    } else if (userName[0] === "B") {
+        return `${giveNicknameWithLetterB()} ${userName}`;
+    } else if (userName[0] === "C") {
+        return `${giveNicknameWithLetterC()} ${userName}`;
+    } else if (userName[0] === "D") {
+        return `${giveNicknameWithLetterD()} ${userName}`;
+    } else if (userName[0] === "E") {
+        return `${giveNicknameWithLetterE()} ${userName}`;
+    } else {
+        return `Work in progress my dear ${userName}...`
     }
 }
 
-console.log(validate("Anton"));
+
+
+console.log(validate("Eva"));
