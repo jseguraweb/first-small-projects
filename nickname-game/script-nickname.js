b1c4dcfc40cef744968f31ea48ef6f9a45790fb1
Jaime Segura <jseguraweb@gmail.com>
2019-08-27 23:07:08 +0200

    link html with css and js

---
+++ nickname-game/script-nickname.js
@@ -0,0 +1,10 @@
+// NICKNAME GAME
+
+// The goal of this JS project is to return the user a random nickname which begins with the same letter that his/her name does.
+// For this I will store 5 nicknames per letter of the alphabet.
+
+const nicknameLetterA = ["Atilla", "Awesome", "Axe", "Abyss", "Amaretto"];
+const nicknameLetterB = ["Bitter", "Barbarian", "Buffy", "Bubble", "Bullet"];
+const nicknameLetterC = ["Crusher", "Creepy", "Cranky", "Cobra", "Coyote"];
+const nicknameLetterD = ["Dirty", "Donut", "Destroyer", "Diablo", "Dragon"];
+const nicknameLetterE = ["Eternal", "Epic", "Eliminator", "Egomaniac", "Explosive"];
