eaae19b84ccad8f47c721bd52812317f6ab2fe81
Jaime Segura <jseguraweb@gmail.com>
2019-08-27 23:46:04 +0200

    add comments and prepare variable for other letters

--- nickname-game/script-nickname.js
+++ nickname-game/script-nickname.js
@@ -3,12 +3,41 @@
 // The goal of this JS project is to return the user a random nickname which begins with the same letter that his/her name does.
 // For this I will store 5 nicknames per letter of the alphabet.
 
+/**
+ * Here we have all the different nickname groups
+ */
+
 const nicknameLetterA = ["Atilla", "Awesome", "Axe", "Abyss", "Amaretto"];
 const nicknameLetterB = ["Bitter", "Barbarian", "Buffy", "Bubble", "Bullet"];
 const nicknameLetterC = ["Crusher", "Creepy", "Cranky", "Cobra", "Coyote"];
 const nicknameLetterD = ["Dirty", "Donut", "Destroyer", "Diablo", "Dragon"];
 const nicknameLetterE = ["Eternal", "Epic", "Eliminator", "Egomaniac", "Explosive"];
+const nicknameLetterF = [];
+const nicknameLetterG = [];
+const nicknameLetterH = [];
+const nicknameLetterI = [];
+const nicknameLetterJ = [];
+const nicknameLetterK = [];
+const nicknameLetterL = [];
+const nicknameLetterM = [];
+const nicknameLetterN = [];
+const nicknameLetterO = [];
+const nicknameLetterP = [];
+const nicknameLetterQ = [];
+const nicknameLetterR = [];
+const nicknameLetterS = [];
+const nicknameLetterT = [];
+const nicknameLetterU = [];
+const nicknameLetterV = [];
+const nicknameLetterW = [];
+const nicknameLetterX = [];
+const nicknameLetterY = [];
+const nicknameLetterZ = [];
 
+/**
+ * The first function which is going to be call is validate() at the very bottom
+ * After it and depending on the first letter of the username, one of the other functions will be called
+ */
 
 function giveNicknameWithLetterA() {
     return nicknameLetterA[Math.floor(Math.random() * 5)];
@@ -30,7 +59,8 @@ function giveNicknameWithLetterE() {
     return nicknameLetterE[Math.floor(Math.random() * 5)];
 }
 
-function validate(userName) {
+function validate(userName) { // TO DO: userName will be removed from the description and converted into a variable inside the function
+    // TO DO: const userName = document.getElementById("nicknameBox").value;
     if (userName[0] === "A") {
         return `${giveNicknameWithLetterA()} ${userName}`;
     } else if (userName[0] === "B") {
@@ -42,7 +72,7 @@ function validate(userName) {
     } else if (userName[0] === "E") {
         return `${giveNicknameWithLetterE()} ${userName}`;
     } else {
-        return `Work in progress my dear ${userName}...`
+        return `Your name is already a challenge my dear ${userName}...`
     }
 }
 
