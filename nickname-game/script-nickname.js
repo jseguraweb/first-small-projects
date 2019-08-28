a683a3b05a37e3286c169086ad054eab328c6f52
Jaime Segura <jseguraweb@gmail.com>
2019-08-28 19:09:06 +0200

    finish all the functions

--- nickname-game/script-nickname.js
+++ nickname-game/script-nickname.js
@@ -59,26 +59,148 @@ function giveNicknameWithLetterE() {
     return nicknameLetterE[Math.floor(Math.random() * 5)];
 }
 
+function giveNicknameWithLetterF() {
+    return nicknameLetterF[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterG() {
+    return nicknameLetterG[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterH() {
+    return nicknameLetterH[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterI() {
+    return nicknameLetterI[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterJ() {
+    return nicknameLetterJ[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterK() {
+    return nicknameLetterK[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterL() {
+    return nicknameLetterL[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterM() {
+    return nicknameLetterM[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterN() {
+    return nicknameLetterN[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterO() {
+    return nicknameLetterO[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterP() {
+    return nicknameLetterP[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterQ() {
+    return nicknameLetterQ[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterR() {
+    return nicknameLetterR[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterS() {
+    return nicknameLetterS[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterT() {
+    return nicknameLetterT[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterU() {
+    return nicknameLetterU[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterV() {
+    return nicknameLetterV[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterW() {
+    return nicknameLetterW[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterX() {
+    return nicknameLetterX[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterY() {
+    return nicknameLetterZ[Math.floor(Math.random() * 5)];
+}
+
+function giveNicknameWithLetterZ() {
+    return nicknameLetterE[Math.floor(Math.random() * 5)];
+}
+
 function validate(userName) { // TO DO: userName will be removed from the description and converted into a variable inside the function
     // TO DO: const userName = document.getElementById("nicknameBox").value;
-    if (userName[0] === "A") {
+    if (userName[0] === "A" || userName[0] === "a") {
         return `${giveNicknameWithLetterA()} ${userName}`;
-    } else if (userName[0] === "B") {
+    } else if (userName[0] === "B" || userName[0] === "b") {
         return `${giveNicknameWithLetterB()} ${userName}`;
-    } else if (userName[0] === "C") {
+    } else if (userName[0] === "C" || userName[0] === "c") {
         return `${giveNicknameWithLetterC()} ${userName}`;
-    } else if (userName[0] === "D") {
+    } else if (userName[0] === "D" || userName[0] === "d") {
         return `${giveNicknameWithLetterD()} ${userName}`;
-    } else if (userName[0] === "E") {
+    } else if (userName[0] === "E" || userName[0] === "e") {
         return `${giveNicknameWithLetterE()} ${userName}`;
+    } else if (userName[0] === "F" || userName[0] === "f") {
+        return `${giveNicknameWithLetterF()} ${userName}`;
+    } else if (userName[0] === "G" || userName[0] === "g") {
+        return `${giveNicknameWithLetterG()} ${userName}`;
+    } else if (userName[0] === "H" || userName[0] === "h") {
+        return `${giveNicknameWithLetterH()} ${userName}`;
+    } else if (userName[0] === "I" || userName[0] === "i") {
+        return `${giveNicknameWithLetterI()} ${userName}`;
+    } else if (userName[0] === "J" || userName[0] === "j") {
+        return `${giveNicknameWithLetterJ()} ${userName}`;
+    } else if (userName[0] === "K" || userName[0] === "k") {
+        return `${giveNicknameWithLetterK()} ${userName}`;
+    } else if (userName[0] === "L" || userName[0] === "l") {
+        return `${giveNicknameWithLetterL()} ${userName}`;
+    } else if (userName[0] === "M" || userName[0] === "m") {
+        return `${giveNicknameWithLetterM()} ${userName}`;
+    } else if (userName[0] === "N" || userName[0] === "n") {
+        return `${giveNicknameWithLetterN()} ${userName}`;
+    } else if (userName[0] === "O" || userName[0] === "o") {
+        return `${giveNicknameWithLetterO()} ${userName}`;
+    } else if (userName[0] === "P" || userName[0] === "p") {
+        return `${giveNicknameWithLetterP()} ${userName}`;
+    } else if (userName[0] === "Q" || userName[0] === "q") {
+        return `${giveNicknameWithLetterQ()} ${userName}`;
+    } else if (userName[0] === "R" || userName[0] === "r") {
+        return `${giveNicknameWithLetterR()} ${userName}`;
+    } else if (userName[0] === "S" || userName[0] === "s") {
+        return `${giveNicknameWithLetterS()} ${userName}`;
+    } else if (userName[0] === "T" || userName[0] === "t") {
+        return `${giveNicknameWithLetterT()} ${userName}`;
+    } else if (userName[0] === "U" || userName[0] === "u") {
+        return `${giveNicknameWithLetterU()} ${userName}`;
+    } else if (userName[0] === "V" || userName[0] === "v") {
+        return `${giveNicknameWithLetterV()} ${userName}`;
+    } else if (userName[0] === "W" || userName[0] === "w") {
+        return `${giveNicknameWithLetterW()} ${userName}`;
+    } else if (userName[0] === "X" || userName[0] === "x") {
+        return `${giveNicknameWithLetterX()} ${userName}`;
+    } else if (userName[0] === "Y" || userName[0] === "y") {
+        return `${giveNicknameWithLetterY()} ${userName}`;
+    } else if (userName[0] === "Z" || userName[0] === "z") {
+        return `${giveNicknameWithLetterZ()} ${userName}`;
     } else {
-        return `Your name is already a challenge my dear ${userName}...`
+        return `Your name is already a challenge, dear ${userName}...`
     }
 }
 
-console.log(validate("Anton"));
-console.log(validate("Berto"));
-console.log(validate("Coral"));
-console.log(validate("Dora"));
-console.log(validate("Eva"));
-console.log(validate("Jaime"));
+console.log(validate("a"));
+
